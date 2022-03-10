f53fa5daaeef72018f888cf70822a15eca56d66f
otienoh <otienojulie@gmail.com>
2022-03-10 04:13:27 +0300

    cleans up

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
@@ -16,16 +17,20 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
+        $user = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@dev.com',
+        ]);
+
         $json = File::get(database_path('data/categories.json'));
 
-        collect(json_decode($json, true))->each(function ($category) {
+        collect(json_decode($json, true))->each(function ($category) use ($user) {
+
             $type = match ($category['type']) {
                 'Income' => Category::TYPE_INCOME,
                 'Expense' => Category::TYPE_EXPENSE,
                 'Transfer' => Category::TYPE_TRANSFER,
             };
-
-            $user = \App\Models\User::first();
 
             $parent = Category::firstOrCreate([
                 'user_id' => $user->id,
@@ -34,14 +39,15 @@ class CategorySeeder extends Seeder
                 'is_hidden' => $category['type'] == 'Transfer',
             ]);
 
-
-            Category::firstOrCreate([
-                'user_id' => $user->id,
-                'parent_id' => $parent->id,
-                'name' =>  $category['category'],
-                'type' => $type,
-                'is_hidden' => $category['type'] == 'Transfer',
-            ]);
+            if (isset($category['category'])) {
+                Category::firstOrCreate([
+                    'user_id' => $user->id,
+                    'parent_id' => $parent->id,
+                    'name' =>  $category['category'],
+                    'type' => $type,
+                    'is_hidden' => $category['type'] == 'Transfer',
+                ]);
+            }
         });
     }
 }
